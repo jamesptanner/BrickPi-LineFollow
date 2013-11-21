27c1167e3c1a3ccf1143c3d502b95542f301da46
jamesptanner <jamesptanner@hotmail.com>
2013-11-21 15:23:19 +0000

    Update configuration
    updated port configs

--- RobotConfig.h
+++ RobotConfig.h
@@ -3,3 +3,17 @@
 //
 
 #include "BrickPiLib.h"
+
+
+#define MOTORR	PORT_A
+#define MOTORL	PORT_B
+#define MOTORNULL1 PORT_C
+#define MOTORNULL2 PORT_D
+
+#define US_PORT PORT_1
+#define CS_PORT PORT_2
+#define KS_PORT PORT_3
+#define NULL_PORT PORT_4
+
+#define TURN_SCALE 20
+#define POWERSCALE 40
